65ecef243a8b4b5e7f93e101e446728541a111ba
Barncastle <spearman.tom@gmail.com>
2019-02-01 10:55:05 +0000

    minfileid support + refactoring

--- TACT.Net/FileLookup/CSVFileLookup.cs
+++ TACT.Net/FileLookup/CSVFileLookup.cs
@@ -11,21 +11,33 @@ namespace TACT.Net.FileLookup
     /// Two column (id,name) CSV lookup
     /// <para>E.g. https://bnet.marlam.in/listfile.php?type=csv</para>
     /// </summary>
-    public class CSVFileLookup : IFileLookup
+    public class CSVFileLookup : IFileLookup, IDisposable
     {
         private Queue<uint> _unusedIds;
         private SemaphoreSlim _sync;
+        private uint _curMaxId;
 
         private readonly string _filename;
+        private readonly uint _minFileId;
         private readonly Dictionary<string, uint> _fileLookup;
 
-        public CSVFileLookup(string filename)
+        #region Constructors
+
+        /// <param name="filepath"></param>
+        /// <param name="minimumFileId">Minimum file ID for new files</param>
+        public CSVFileLookup(string filepath, uint minimumFileId = 0)
         {
-            _filename = filename;
+            _minFileId = minimumFileId;
+            _filename = filepath;
             _fileLookup = new Dictionary<string, uint>(StringComparer.OrdinalIgnoreCase);
-            _sync = new SemaphoreSlim(0, 1);
+            _sync = new SemaphoreSlim(1, 1);
+
+            LoadUnusedIDs();
         }
 
+        #endregion
+
+        #region IO
 
         public void Open()
         {
@@ -44,7 +56,8 @@ namespace TACT.Net.FileLookup
                 }
             }
 
-            LoadUnusedIDs();
+            // store the current max id/wanted max id
+            _curMaxId = Math.Max(_fileLookup.Values.Max(), _minFileId);
         }
 
         public async Task Sync()
@@ -64,31 +77,54 @@ namespace TACT.Net.FileLookup
 
         public void Close()
         {
-            Task.Run(Sync);
+            Task.Run(Sync).Wait();
         }
 
+        #endregion
+
+        #region Methods
 
         public uint GetOrCreateFileId(string filename)
         {
+            // attempt to get the fileid
             if (!_fileLookup.TryGetValue(filename, out uint id))
             {
-                id = _unusedIds.Count > 0 ? _unusedIds.Dequeue() : (uint)_fileLookup.Count;
+                // attempt to load an unusedid
+                if (!_unusedIds.TryDequeue(out id))
+                    id = ++_curMaxId; // used the next highest id
+
                 _fileLookup.Add(filename, id);
             }
 
             return id;
         }
 
+        #endregion
+
+        #region Helpers
+
         /// <summary>
         /// Generates a range of IDs that aren't used
         /// </summary>
         private void LoadUnusedIDs()
         {
-            var idRange = Enumerable.Range((int)_fileLookup.Values.Min(), (int)_fileLookup.Values.Max())
-                                    .Select(x => (uint)x)
-                                    .Except(_fileLookup.Values);
+            uint min = Math.Max(_fileLookup.Values.Min(), _minFileId);
 
+            IEnumerable<uint> GetRange()
+            {
+                for (; min < _curMaxId; min++)
+                    yield return min;
+            }
+
+            var idRange = GetRange().Except(_fileLookup.Values);
             _unusedIds = new Queue<uint>(idRange);
         }
+
+        public void Dispose()
+        {
+            ((IDisposable)_sync).Dispose();
+        }
+
+        #endregion
     }
 }
