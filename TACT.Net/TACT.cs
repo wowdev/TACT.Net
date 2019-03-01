ee82d5d379cd7f45387106b8e30b0a8b1ab8ffe9
Barncastle <spearman.tom@gmail.com>
2019-03-01 19:57:16 +0000

    wip TACT methods

--- TACT.Net/TACT.cs
+++ TACT.Net/TACT.cs
@@ -1,4 +1,7 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("TACT.Net.Tests")]
 namespace TACT.Net
@@ -32,7 +35,8 @@ namespace TACT.Net
         #region Methods
 
         /// <summary>
-        /// Creates a new TACT container populated with the necessary base files
+        /// Creates a new TACT container populated with: defaulted configs, an index container 
+        /// and an empty root, encoding, install and download file
         /// </summary>
         /// <param name="product"></param>
         /// <param name="locale"></param>
@@ -42,29 +46,75 @@ namespace TACT.Net
             ConfigContainer.Create();
 
             IndexContainer = new Indices.IndexContainer();
-            RootFile = new Root.RootFile(this);
+            RootFile = new Root.RootFile();
             EncodingFile = new Encoding.EncodingFile();
             InstallFile = new Install.InstallFile();
             DownloadFile = new Download.DownloadFile();
         }
 
-        ///// <summary>
-        ///// Opens an existing TACT container and loads the Root and Encoding files
-        ///// </summary>
-        ///// <param name="directory"></param>
-        ///// <param name="product"></param>
-        ///// <param name="locale"></param>
-        //public void Open(string directory, string product, Locale locale)
-        //{
-        //    var configContainer = new Configs.ConfigContainer(product, this);
-        //    configContainer.Open(directory, locale);
+        /// <summary>
+        /// Opens an existing TACT container and loads the Root and Encoding files
+        /// </summary>
+        /// <param name="directory"></param>
+        /// <param name="product"></param>
+        /// <param name="locale"></param>
+        public void Open(string directory, string product, Locale locale)
+        {
+            ConfigContainer = new Configs.ConfigContainer(product, locale);
+            ConfigContainer.OpenLocal(directory);
 
-        //    var archiveContainer = new Archives.ArchiveContainer(this);
-        //    archiveContainer.Open(directory);
+            IndexContainer = new Indices.IndexContainer();
+            IndexContainer.Open(directory);
 
-        //    new Root.RootFile(archiveContainer.OpenFile(configContainer.RootMD5), this);
-        //    new Encoding.EncodingFile(archiveContainer.OpenFile(configContainer.EncodingEKey), this);
-        //}
+            if (!ConfigContainer.EncodingEKey.IsEmpty)
+            {
+                EncodingFile = new Encoding.EncodingFile(BaseDirectory, ConfigContainer.EncodingEKey);
+
+                if (EncodingFile.TryGetContentEntry(ConfigContainer.RootMD5, out var rootCEntry))
+                    RootFile = new Root.RootFile(BaseDirectory, rootCEntry.EKey);
+            }                
+        }
+
+        public void Save(string directory)
+        {
+            IndexContainer?.Save(directory, ConfigContainer);
+            RootFile?.Write(directory, this);
+            DownloadFile?.Write(directory, this);
+            DownloadSizeFile?.Write(directory, this);
+            InstallFile?.Write(directory, this);
+            EncodingFile?.Write(directory, ConfigContainer);
+            ConfigContainer?.Save(directory);
+        }
+
+        public void Clean()
+        {
+             var usedHashes = new HashSet<Cryptography.MD5Hash>();
+
+            if(ConfigContainer != null)
+            {
+                usedHashes.Add(ConfigContainer.BuildConfigMD5);
+                usedHashes.Add(ConfigContainer.CDNConfigMD5);
+                usedHashes.Add(ConfigContainer.PatchConfigMD5);
+                usedHashes.Add(ConfigContainer.RootMD5);
+                usedHashes.Add(ConfigContainer.EncodingMD5);
+                usedHashes.Add(ConfigContainer.InstallMD5);
+                usedHashes.Add(ConfigContainer.DownloadMD5);
+                usedHashes.Add(ConfigContainer.DownloadSizeMD5);
+                usedHashes.Add(ConfigContainer.PatchMD5);
+            }            
+
+            if(RootFile != null)
+            {
+                var blocks = RootFile.GetBlocks(Root.LocaleFlags.All_WoW, Root.ContentFlags.None);
+                foreach (var block in blocks)
+                    usedHashes.UnionWith(block.Records.Select(x => x.Value.CKey));
+            }
+
+            if(InstallFile != null)
+            {
+                usedHashes.UnionWith(InstallFile.Files.Select(x => x.CKey));
+            }
+        }
 
 
         #endregion
