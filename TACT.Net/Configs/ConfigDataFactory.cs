b7b0b84dab86a50ed48d4353000925dd78a92dcf
Barncastle <spearman.tom@gmail.com>
2019-03-06 15:06:45 +0000

    more configdata changes - needs rewriting completely

--- TACT.Net/Configs/ConfigDataFactory.cs
+++ TACT.Net/Configs/ConfigDataFactory.cs
@@ -13,84 +13,90 @@ namespace TACT.Net.Configs
 
         public static Dictionary<string, List<string>> GenerateData(ConfigType type, uint build)
         {
-            var data = new Dictionary<string, List<string>>(StringComparer.OrdinalIgnoreCase);
+            var collection = new Dictionary<string, List<string>>(StringComparer.OrdinalIgnoreCase);
 
             switch (type)
             {
                 case ConfigType.BuildConfig:
-                    AddValue(data, "build-name", "");
-                    AddValue(data, "build-uid", "");
-                    AddValue(data, "build-product", "WoW");
-                    AddValue(data, "build-playbuild-installer", "ngdptool_casc2");
-                    AddValue(data, "root", "");
-                    AddValue(data, "install", "", "");
-                    AddValue(data, "download", "", "");
-                    AddValue(data, "encoding", "", "");
+                    AddValue(collection, "build-name", "");
+                    AddValue(collection, "build-uid", "");
+                    AddValue(collection, "build-product", "WoW");
+                    AddValue(collection, "build-playbuild-installer", "ngdptool_casc2");
+                    AddValue(collection, "root", "");
+                    AddValue(collection, "install", "", "");
+                    AddValue(collection, "download", "", "");
+                    AddValue(collection, "encoding", "", "");
                     break;
                 case ConfigType.CDNConfig:
-                    AddValue(data, "archives");
-                    AddValue(data, "archives-index-size");
-                    AddValue(data, "patch-archives");
-                    AddValue(data, "patch-archives-index-size");
+                    AddValue(collection, "archives");
+                    AddValue(collection, "archives-index-size");
+                    AddValue(collection, "patch-archives");
+                    AddValue(collection, "patch-archives-index-size");
                     break;
                 default:
                     throw new ArgumentException("Invalid KeyValueConfig type");
             }
 
             // build specific fields
-            if (Lookup.Value.TryGetValue(type, out var fields))
-            {
-                string[] defaultValue = type == ConfigType.BuildConfig ? new[] { "", "" } : new string[0];
+            AddLookupValues(collection, type, build);            
 
-                foreach (var field in fields)
-                    if (build > field.Value)
-                        AddValue(data, field.Key, defaultValue);
-            }
-
-            return data;
+            return collection;
         }
 
         public static (string[] Fields, string[] Values) GenerateData(ConfigType type)
         {
-            var data = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
+            var collection = new Dictionary<string, string>();
 
             switch (type)
             {
                 case ConfigType.CDNs:
-                    AddValue(data, "Name!STRING:0");
-                    AddValue(data, "Path!STRING:0", "tpr/wow");
-                    AddValue(data, "Hosts!STRING:0");
-                    AddValue(data, "Servers!STRING:0");
-                    AddValue(data, "ConfigPath!STRING:0", "tpr/configs/data");
+                    AddValue(collection, "Name!STRING:0");
+                    AddValue(collection, "Path!STRING:0", "tpr/wow");
+                    AddValue(collection, "Hosts!STRING:0");
+                    AddValue(collection, "Servers!STRING:0");
+                    AddValue(collection, "ConfigPath!STRING:0", "tpr/configs/data");
                     break;
                 case ConfigType.Versions:
-                    AddValue(data, "Region!STRING:0");
-                    AddValue(data, "BuildConfig!HEX:16");
-                    AddValue(data, "CDNConfig!HEX:16");
-                    AddValue(data, "KeyRing!HEX:16");
-                    AddValue(data, "BuildId!DEC:4", "00000");
-                    AddValue(data, "VersionsName!String:0", "0.0.0.00000");
-                    AddValue(data, "ProductConfig!HEX:16");
+                    AddValue(collection, "Region!STRING:0");
+                    AddValue(collection, "BuildConfig!HEX:16");
+                    AddValue(collection, "CDNConfig!HEX:16");
+                    AddValue(collection, "KeyRing!HEX:16");
+                    AddValue(collection, "BuildId!DEC:4", "00000");
+                    AddValue(collection, "VersionsName!String:0", "0.0.0.00000");
+                    AddValue(collection, "ProductConfig!HEX:16");
                     break;
                 default:
                     throw new ArgumentException("Invalid VariableConfig type");
             }
 
-            return (data.Keys.ToArray(), data.Values.ToArray());
+            return (collection.Keys.ToArray(), collection.Values.ToArray());
         }
 
         #endregion
 
         #region Helpers
 
-        private static void AddValue(Dictionary<string, List<string>> collection, string key, params string[] values)
+        private static void AddValue(System.Collections.IDictionary dictionay, string key, params string[] values)
         {
-            collection[key] = values.Length == 0 ? new List<string>() : new List<string>(values);
+            var valueType = dictionay.GetType().GetGenericArguments()[1];
+            bool isList = valueType.GetGenericTypeDefinition() == typeof(List<>);
+
+            if (isList)
+                dictionay[key] = new List<string>(values);
+            else
+                dictionay[key] = values.Length == 0 ? "" : values[1];            
         }
 
-        private static void AddValue(Dictionary<string, string> collection, string key, string value = "")
+        private static void AddLookupValues(Dictionary<string, List<string>> collection, ConfigType type, uint build)
         {
-            collection[key] = value;
+            if (Lookup.Value.TryGetValue(type, out var fields))
+            {
+                string[] defaultValue = type == ConfigType.BuildConfig ? new[] { "", "" } : new string[0];
+
+                foreach (var field in fields)
+                    if (build > field.Value)
+                        AddValue(collection, field.Key, defaultValue);
+            }
         }
 
         #endregion
