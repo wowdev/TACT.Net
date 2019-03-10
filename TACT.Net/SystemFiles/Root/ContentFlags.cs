4decf009725c68edb2ef9d804c174cbe1810143f
Barncastle <spearman.tom@gmail.com>
2019-03-10 13:03:40 +0000

    more flags

--- TACT.Net/SystemFiles/Root/ContentFlags.cs
+++ TACT.Net/SystemFiles/Root/ContentFlags.cs
@@ -6,7 +6,10 @@ namespace TACT.Net.Root
     public enum ContentFlags : uint
     {
         None = 0,
+        Windows = 0x8, // HLSL shaders + updateplugin.dll
+        Mac = 0x10, // MTL shaders + updateplugin.dylib
         LowViolence = 0x80, // many models have this flag
+        Unknown0x800 = 0x800, // updateplugin.dll + updateplugin.dylib
         Bundle = 0x40000000,
         NoCompression = 0x80000000, // sounds have this flag
     }
