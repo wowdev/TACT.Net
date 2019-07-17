1cf34818f92a8bb5083509fceff41ca199d9873a
Barncastle <spearman.tom@gmail.com>
2019-07-17 20:30:16 +0100

    updated content flags

--- TACT.Net/SystemFiles/Root/ContentFlags.cs
+++ TACT.Net/SystemFiles/Root/ContentFlags.cs
@@ -6,12 +6,41 @@ namespace TACT.Net.Root
     public enum ContentFlags : uint
     {
         None = 0,
-        Windows = 0x8, // HLSL shaders + updateplugin.dll
-        Mac = 0x10, // MTL shaders + updateplugin.dylib
-        LowViolence = 0x80, // many models have this flag
-        Unknown_800 = 0x800, // updateplugin.dll + updateplugin.dylib
+        /// <summary>
+        /// e.g. HLSL shaders
+        /// </summary>
+        Windows = 0x8,
+        /// <summary>
+        /// e.g. MTL shaders
+        /// </summary>
+        Mac = 0x10,
+        /// <summary>
+        /// Many models have this flag
+        /// </summary>
+        LowViolence = 0x80,
+        /// <summary>
+        /// UpdatePlugin.dll + UpdatePlugin.dylib
+        /// </summary>
+        Unknown_800 = 0x800,
+        /// <summary>
+        /// Files require a TACT key to be viewed
+        /// </summary>
+        Encrypted = 0x8000000,
+        /// <summary>
+        /// Files do not have a namehash
+        /// </summary>
+        NoNameHash = 0x10000000,
+        /// <summary>
+        /// All non-1280px wide Cinematics have this
+        /// </summary>
+        UncommonResolution = 0x20000000,
+        /// <summary>
+        /// 
+        /// </summary>
         Bundle = 0x40000000,
-        NoCompression = 0x80000000, // sounds have this flag
-        NoNameHash = 0x10000000
+        /// <summary>
+        /// Sounds have this flag
+        /// </summary>
+        Uncompressed = 0x80000000,
     }
 }
